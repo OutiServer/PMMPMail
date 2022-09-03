9806e39a2cf798395131efd0e4841617ff5f1a03
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-09-04 02:54:13 +0900

    メールを管理する部分を追加

--- src/Database/Mail/MailData.php
+++ src/Database/Mail/MailData.php
@@ -62,11 +62,27 @@ class MailData extends BaseData
     }
 
     /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
      * @return string
      */
     public function getContent(): string
     {
         return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
     }
 
     /**
